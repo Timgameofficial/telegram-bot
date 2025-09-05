ecd87037b39867bc91e39a2cbd9120c9c1be123f
Timgameofficial <satyrn2285@gmail.com>
2025-09-05 20:27:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -153,6 +153,5 @@ def set_webhook():
     else:
         return "Ошибка установки webhook"
 
-# ====== Запуск ======
 if name == "main":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), threaded=False)
